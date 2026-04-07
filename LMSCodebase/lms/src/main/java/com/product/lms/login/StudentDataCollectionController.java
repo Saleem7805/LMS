119c78dde0586473d7b6d360535d9205840052a7
= <94Saleem@gmail.com>
2026-04-07 16:28:55 +0530

    Committing all the files

--- LMSCodebase/lms/src/main/java/com/product/lms/login/StudentDataCollectionController.java
+++ LMSCodebase/lms/src/main/java/com/product/lms/login/StudentDataCollectionController.java
@@ -3,11 +3,13 @@ package com.product.lms.login;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins="*")
 public class StudentDataCollectionController {
 	
 	@Autowired
